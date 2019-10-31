346dc0ed3d40e314fd639f68132154182250ab5e
diegoasterio <diegoasterio@correo.ugr.es>
2019-10-31 15:20:01 +0100

    structuring comments

--- startDocker.sh
+++ startDocker.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
 docker build . -t go-web-app # builds up our image and calls it go-web-app
-docker run \
-       -p 8080:5000 \ # HOSTPORT:CONTPORT
-       -v=`pwd`/flask:/flask \ # bind volume ./flask to /flask in the container
-       go-web-app 
+# -p := HOSTPORT:CONTPORT
+# -v := bind volume ./flask to /flask in the container
+docker run -p 8080:5000 -v=`pwd `/flask:/flask go-web-app 
