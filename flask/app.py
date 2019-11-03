25c09e9a3ee0631d7a9e1c856e9ae4dd5ddc9594
diegoasterio <diegoasterio@correo.ugr.es>
2019-11-03 20:27:02 +0100

    managing players to the database

--- flask/app.py
+++ flask/app.py
@@ -1,5 +1,7 @@
 import pprint
 
+import json
+
 from flask import Flask, render_template, session, request, redirect
 
 from pickleshare import *
@@ -13,6 +15,36 @@ mongodb= client.goPlayers
 app = Flask(__name__)
 app.secret_key = b'2V2XNWcP0L4rD2vKykr69rlBmnYkAAzdpn2gBwFHNKE='
 
+def getCountries():
+    try:
+        fn = open("./static/slim-3.json","U")
+    except IOError:
+        return ["Something went wrong"]
+    ISO3166 = json.load(fn)
+    ret = []
+    for country in ISO3166:
+        ret.append(country['alpha-3'])
+    return ret
+    
+
+def addusername():
+    params = dict()
+    if 'email' in session:
+        params['email'] = session['email']
+    return params
+
+def valid_user(name):
+    if not name in db:
+        return True
+    return False
+
+def check_name(name):
+    if mongodb.ugonet.count_documents({'key_name':name}) > 0:
+        return False
+    else:
+        return True
+                                  
+
 @app.before_request
 def before_request():
     if not 'history' in session:
@@ -22,32 +54,29 @@ def before_request():
 
 @app.route('/')
 def print_index():
-    email = None
-    if 'email' in session:
-        email = session['email']
-    return render_template('index.html',email=email, P1=session['history'][0],P2=session['history'][1],P3=session['history'][2])
+    params = addusername()
+    params['history'] = session['history'][0:3]
+    return render_template('index.html',params=params)
 
 @app.route('/ratings')
 def print_ratings():
-    return render_template('ratings.html')
+    params = addusername()
+    return render_template('ratings.html',params=params)
 
 @app.route('/about')
 def print_about():
-    return render_template('about.html')
+    params=addusername()
+    return render_template('about.html',params=params)
 
 @app.route('/contact')
 def print_contact():
-    return render_template('contact.html')
-
-def valid_user(name):
-    if not name in db:
-        return True
-    return False
+    params=addusername()
+    return render_template('contact.html',params=params)
 
 @app.route('/register', methods=['POST', 'GET'])
 def print_reg():
-    error = None
-    user = None
+    params=addusername()
+    params['error'] = None
     if request.method == 'POST':
         user = request.form['exampleInputEmail1']
         passwd = request.form['exampleInputPassword1']
@@ -55,8 +84,8 @@ def print_reg():
             db[user] = {'pass': passwd}
             return redirect('/')
         else:
-            error = 'Username already chosen'
-    return render_template('register.html', user=user, regerror=error)
+            params['error'] = 'Username already chosen'
+    return render_template('register.html', params=params)
 
 def valid_login(name, passw):
     if name in db:
@@ -65,17 +94,19 @@ def valid_login(name, passw):
 
 @app.route('/login', methods=['POST', 'GET'])
 def print_login():
-    error = None
-    user = None
+    params = addusername()
+    params['error'] = None
+    params['user'] = None
     if request.method == 'POST':
-        if valid_login(request.form['exampleInputEmail1'],
-                       request.form['exampleInputPassword1']):
+        params['user'] = request.form['exampleInputEmail1']
+        passwd = request.form['exampleInputPassword1']
+        if valid_login(params['user'],passwd):
+
             session['email']=request.form['exampleInputEmail1']
             return redirect('/')
         else:
-            error = 'Invalid username/password'
-        user = request.form['exampleInputEmail1']
-    return render_template('login.html',loginerror=error,user=user)
+            params['error'] = 'Invalid username/password'
+    return render_template('login.html',params=params)
     
 @app.route('/logout')
 def erase_session():
@@ -84,7 +115,7 @@ def erase_session():
 
 @app.route('/statistics')
 def show_statistics():
-    params = dict()
+    params = addusername()
     numberOfSpaniards = mongodb.ugonet.count_documents({"citizenship":"ESP"})
     vals = mongodb.ugonet.find({"citizenship":"ESP"})
     pprint.pprint(vals)
@@ -94,10 +125,43 @@ def show_statistics():
     params['names'] = names
     return render_template('statistics.html',params=params)
     
+@app.route('/documents', methods=['POST', 'GET'])
+def show_docs():
+    params = addusername()
+    playersAdded = mongodb.ugonet.find({"creator":params['email']})
+    params['names'] = []
+    params['countries'] = getCountries()
+    for player in playersAdded:
+        name = player['key_name']
+        params['names'].append(name)
+    if request.method == 'POST':
+        name = request.form['name']
+        sex = request.form['sex']
+        citizenship = request.form['citizenship']
+        if check_name(name):
+            mongodb.ugonet.insert({
+                'key_name' : name,
+                'sex' : sex,
+                'citizenship' : citizenship,
+                'creator' : params['email']
+                })
+            return redirect('/documents')
+        else:
+            params['error'] = "Introduced player already exists"
+    return render_template('documents.html',params=params)
+
+@app.route('/erase', methods=['POST'])
+def erase_player():
+    pprint.pprint(request.form)
+    name = request.form['erasename']
+    mongodb.ugonet.remove({'key_name':name})
+    return redirect('/documents')
+    
+    
 
 @app.route('/mongo')
 def mongo():
-    val = db.ugonet.find_one()
+    val = mongodb.ugonet.find_one()
     pprint.pprint(val)
     numberOfSpaniards = mongodb.ugonet.count_documents({"citizenship":"ESP"})
     return val['key_name']
